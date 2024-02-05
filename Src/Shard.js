e20a6cb74c742804bb827b755e733653783da3a0
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-02-05 12:29:23 +0530

    Update Shard.js

--- Src/Shard.js
+++ Src/Shard.js
@@ -4,7 +4,7 @@ const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager("./Src/Saavan.js", {
     respawn: true,
     autoSpawn: true,
-    token: "MTA0NDU5NjA1MDg1OTY2MzQwMQ.GYplDg.3u-IRyEB3O7gxIxnlk_LY2-mMkwu9u2v98gh38",
+    token: "paste your bot token",
     totalShards: 1,
     shardList: "auto",
 });
